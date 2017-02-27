f6bc59e25276a6f320e934a9f8011f5b5ea65683
Wei Ket <wei_ket@hotmail.com>
2017-02-27 21:44:29 +0800

    Ticket show images

--- app/Models/Services/TicketService.php
+++ app/Models/Services/TicketService.php
@@ -11,8 +11,9 @@ class TicketService
 {
   public function getTicket($ticket_id) {
     $ticket = Ticket::findOrNew($ticket_id);
-    $ticket->staff_assignment = DB::table('staff_assignment')->where('ticket_id', $ticket_id)->get();
-    $ticket->preferred_datetime = DB::table('ticket_preferred_datetime')->where('ticket_id', $ticket_id)->get();
+    $ticket->staff_assignments = DB::table('staff_assignment')->where('ticket_id', $ticket_id)->get();
+    $ticket->preferred_datetimes = DB::table('ticket_preferred_datetime')->where('ticket_id', $ticket_id)->get();
+    $ticket->images = DB::table('ticket_image')->where('ticket_id', $ticket_id)->get();
     return $ticket;
   }
 }
--- resources/views/ticket/form.blade.php
+++ resources/views/ticket/form.blade.php
@@ -47,6 +47,24 @@
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
+                        <label class="control-label col-md-3">Category</label>
+                      <div class="col-md-9">
+                        {{Form::text('name', $ticket->name, ['class'=>'form-control'])}}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label class="control-label col-md-3">Status</label>
+                      <div class="col-md-9">
+                        {{Form::select('stat', TicketStat::$values, $ticket->stat, ['class'=>'form-control', 'placeholder'=>''])}}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
                       <label class="control-label col-md-3">Company</label>
                       <div class="col-md-9">
                         {{Form::select('stat', $companies, $ticket->stat, ['class'=>'form-control', 'placeholder'=>''])}}
@@ -101,6 +119,36 @@
                 <div class="row">
                   <div class="col-md-12">
                     <div class="form-group">
+                      <label class="control-label col-md-2">Images</label>
+                      <div class="col-md-10">
+                        <table class="table table-hover table-bordered">
+                          <thead>
+                          <tr>
+                            <th>Image</th>
+                            <th>Problem</th>
+                            <th>Description</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          @foreach($ticket->images as $i)
+                            <tr>
+                              <td>
+                                <img src="{{asset('images/'.$i->image)}}" class="ticket-image"><br>
+                                <input type="file">
+                              </td>
+                              <td><textarea class="form-control"></textarea></td>
+                              <td><textarea class="form-control"></textarea></td>
+                            </tr>
+                          @endforeach
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
                       <label class="control-label col-md-2">Preferred Date Time</label>
                       <div class="col-md-10">
                         <table class="table table-hover table-bordered">
@@ -111,7 +159,7 @@
                           </tr>
                           </thead>
                           <tbody>
-                          @foreach($ticket->preferred_datetime as $p)
+                          @foreach($ticket->preferred_datetimes as $p)
                             <tr>
                               <td>
                                 {{ViewHelper::formatDate($p->date_from)}}
@@ -143,7 +191,7 @@
                           </tr>
                           </thead>
                           <tbody>
-                          @foreach($ticket->staff_assignment as $a)
+                          @foreach($ticket->staff_assignments as $a)
                             <tr>
                               <td>{{$a->staff_id}}</td>
                               <td>
