15ed2c229d46b19bf3c493d3c2d18ab8010c56a2
Wei Ket Ong <weiket.ong@titansoft.com.sg>
2018-02-28 19:12:30 +0800

    Admin create ticket show 1 issue and preferred slot

--- app/Http/Controllers/Admin/TicketController.php
+++ app/Http/Controllers/Admin/TicketController.php
@@ -12,6 +12,7 @@ use App\Models\Services\PaymentService;
 use App\Models\Services\TicketService;
 use App\Models\Skill;
 use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Helpers\BackendHelper;
 use ViewHelper;
@@ -74,7 +75,11 @@ class TicketController extends Controller
         $result = "Ticket " . $data['action'] . "d";
       }
       return redirect('admin/ticket/save/' . $ticket_id)->with('msg', $result);
-      
+    }
+  
+    if($data['action'] == "create") {
+      $ticket->issues = '[{"image":"", "issue_desc":"", "expected_desc":"", "stat":"add"}]';
+      $ticket->preferred_slots = '[{"date": "'.Carbon::now()->format('d M Y').'", "time_start":"", "time_end":"", "stat":"add"}]';
     }
     $data['ticket'] = $ticket;
     $data['companies'] = Company::getCompanyDropdown();
